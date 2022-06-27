337a25252241a4d7398745d1155f5534f61f685c
Joy <parkerjn90@gmail.com>
2022-06-27 12:01:30 -0400

    write skeleton for each subcomponent within MyCollection

---
+++ client/src/components/UserMain/MyCollection/MyCollectionList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyCollectionTile from './MyCollectionTile';
+
+function MyCollectionList() {
+  return (
+    <div>
+      <MyCollectionTile />
+    </div>
+  );
+}
+
+export default MyCollectionList;
