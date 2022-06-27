87607f4b917edcf305e61248597bdfc3ee8b44d6
Philip Koller <philipkoller@Philips-MacBook-Pro.local>
2022-06-27 16:40:46 -0500

    added models folder, added controllers

---
+++ database/Models/games.js
@@ -0,0 +1,14 @@
+const { pool } = require('../index');
+
+const getAll = function() get{
+ //TODO: write query to get all games form db
+}
+
+const getOne = function(game_id) get{
+ //TODO: write query to single game form db
+}
+
+module.exports = {
+  getAll: getAll,
+  getOne: getOne
+}
