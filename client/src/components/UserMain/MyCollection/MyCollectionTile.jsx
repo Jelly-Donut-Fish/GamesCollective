a0b395180b8f7b5beea1e4205f8bd042dc01b44b
Joy <parkerjn90@gmail.com>
2022-06-27 14:02:28 -0400

    add details and icons to page, work on adding label for drop down selection for game status

--- client/src/components/UserMain/MyCollection/MyCollectionTile.jsx
+++ client/src/components/UserMain/MyCollection/MyCollectionTile.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { MdComment } from 'react-icons/md';
+import { FaTrashAlt } from 'react-icons/fa';
 
 function MyCollectionTile() {
   return (
     <div>
+      <img src="https://en.wikipedia.org/wiki/Sly_Cooper#/media/File:Sly_Cooper_series.png" alt="[Game Title] thumbnail" />
       <h3>Game Title</h3>
+      <span>[Release Date]</span>
+      <h4>Publisher/Studio</h4>
+      <span>Platforms Available</span>
+      <span>genre, genre, genre</span>
+      <p>Rating</p>
+      <label htmlFor="game status">
+        <select></select>
+      </label>
+      <p><MdComment /></p>
+      <p><FaTrashAlt /></p>
     </div>
   );
 }
