fdeb8fe005c6c598e32546863c8e0b6a2e48112e
bricekoppin <bricekoppinesq@gmail.com>
2022-06-27 15:38:34 -0400

    Redux setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,12 @@
-/* eslint-disable import/extensions */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPageContainer from '../containers/LandingPageContainer.js';
 import UserMainContainer from '../containers/UserMainContainer.js';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route exact path="/"><LandingPage /></Route> */}
-        <Route path="/user"><UserMainContainer /></Route>
-        <Route path="/landingpage"><LandingPageContainer /></Route>
+        <Route path="/" element={<UserMainContainer />} />
       </Routes>
     </Router>
   );
