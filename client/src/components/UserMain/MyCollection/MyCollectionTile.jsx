4faa3734280738736ce8e5c693064fbc98e1e640
Joy <parkerjn90@gmail.com>
2022-06-27 16:05:37 -0400

    remove 'add comment' pieces of code

--- client/src/components/UserMain/MyCollection/MyCollectionTile.jsx
+++ client/src/components/UserMain/MyCollection/MyCollectionTile.jsx
@@ -2,30 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { MdComment } from 'react-icons/md';
 import { FaTrashAlt } from 'react-icons/fa';
-import GameComment from './GameComment';
 
 function MyCollectionTile() {
-  const [commentOpen, setCommentOpen] = useState(false);
-  const [comment, setComment] = useState('');
-  // open comment modal
-  const showCommentModal = (e) => {
-    e.preventDefault();
-    setCommentOpen(!commentOpen);
-  };
-
-  // comment - put request
-  const addComment = (e) => {
-    e.preventDefault();
-    axios.put('[insert endpoint here]', { data: { review: comment } })
-      .then(() => {
-
-      })
-      .catch((err) => {
-        console.error('Comment was not successfully added:');
-        console.log(err);
-      });
-  };
-
   // delete request
   const removeFromCollection = (e) => {
     e.preventDefault();
@@ -58,9 +36,8 @@ function MyCollectionTile() {
           <option value="Backlog">Haven&apos;t Started</option>
         </select>
       </label>
-      <button type="button" onClick={showCommentModal}><MdComment /></button>
-      <button type="button" onClick={removeFromCollection}><FaTrashAlt /></button>
-      {commentOpen && <GameComment showCommentModal={showCommentModal}/>}
+      <p><MdComment /></p>
+      <p onClick={removeFromCollection}><FaTrashAlt /></p>
     </div>
   );
 }
