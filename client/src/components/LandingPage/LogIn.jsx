516e40dc1931b7f1a30a3371830a0dc13ac3d71f
Benjamin Thornton <benjamin.l.thornton@gmail.com>
2022-06-27 14:27:23 -0500

    login component start

---
+++ client/src/components/LandingPage/LogIn.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+export default function LogIn() {
+  return (
+    <div>
+
+    </div>
+  );
+}
