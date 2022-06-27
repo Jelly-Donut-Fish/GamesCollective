17a3aa17ab8b4b1802889362b5cb95047501dd29
Joy <parkerjn90@gmail.com>
2022-06-27 15:18:02 -0400

    write game comment modal to allow users to input their comment on the game

---
+++ client/src/components/UserMain/MyCollection/GameComment.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GameComment() {
+  return (
+    <div>
+      <label htmlFor="add comment">
+        <textarea id="add comment" name="comment" rows="5" cols="70" placeholder="Add your comment here" />
+      </label>
+    </div>
+  );
+}
+
+export default GameComment;
