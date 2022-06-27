67f71f7ad3de798904a7ca7a4bcbe6033475a8b3
Joy <parkerjn90@gmail.com>
2022-06-27 14:01:46 -0400

    write drop down select menu for filter

--- client/src/components/UserMain/MyCollection/MyCollectionSearch.jsx
+++ client/src/components/UserMain/MyCollection/MyCollectionSearch.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 function MyCollectionSearch() {
   return (
     <div>
-      <input type="text" placeholder="Filter Collection" />
+      <label htmlFor="user filter">
+        <select name="filters" id="user filter">
+          <option value="">Filter by</option>
+          <option value="title">Title</option>
+          <option value="genre">Genre</option>
+          <option value="category">Category</option>
+          <option value="status">Status</option>
+          <option value="platform">Platform</option>
+          <option value="service">Game Library</option>
+        </select>
+      </label>
+      <label htmlFor="filter collection">
+        <input type="text" placeholder="Filter Collection" id="filter collection" />
+      </label>
     </div>
   );
 }
