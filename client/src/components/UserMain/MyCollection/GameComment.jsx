fb73b9c6c4399c72e9c0d8e1b828ac23b13ce7b6
Joy <parkerjn90@gmail.com>
2022-06-27 15:19:35 -0400

    add buttons to trigger axios request or close and cancel adding a comment

--- client/src/components/UserMain/MyCollection/GameComment.jsx
+++ client/src/components/UserMain/MyCollection/GameComment.jsx
@@ -6,6 +6,8 @@ function GameComment() {
       <label htmlFor="add comment">
         <textarea id="add comment" name="comment" rows="5" cols="70" placeholder="Add your comment here" />
       </label>
+      <button type="button">Add Comment</button>
+      <button type="button">Cancel</button>
     </div>
   );
 }
