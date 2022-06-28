289b934b2d760bfe33a157958360c843c8fad146
Joy <parkerjn90@gmail.com>
2022-06-27 20:57:59 -0400

    add mapping functions to add all categories/genres/etc within user's list to drop down menus

--- client/src/components/UserMain/MyCollection/MyCollectionSearch.jsx
+++ client/src/components/UserMain/MyCollection/MyCollectionSearch.jsx
@@ -3,12 +3,10 @@ import { AiOutlineSearch } from 'react-icons/ai';
 
 function MyCollectionSearch({ filterMyCollection }) {
   const [search, setSearch] = useState('');
-  const [filter, setFilter] = useState('');
-
   // set submit handler
   const submitSearch = (e) => {
     e.preventDefault();
-    filterMyCollection(search, filter);
+    filterMyCollection(search);
   };
 
   // set search string
@@ -21,28 +19,53 @@ function MyCollectionSearch({ filterMyCollection }) {
   const filterHandler = (e) => {
     e.preventDefault();
     setFilter(e.target.value);
+    // call function to lift state here
   };
+
+  const genres = ['yellow', 'blue', 'green'];
 
   return (
     <div>
-      <label htmlFor="user filter">
-        <select name="filters" id="user filter" onChange={filterHandler}>
-          <option value="">Filter options</option>
-          <option value="title">Title</option>
-          <option value="genre">Genre</option>
-          <option value="category">Category</option>
-          <option value="status">Status</option>
-          <option value="platform">Platform</option>
-          <option value="service">Game Library</option>
-        </select>
+      <label htmlFor="title">
+        <input type="text" placeholder="Search for a title" id="title" onChange={searchHandler} />
       </label>
-      <label htmlFor="filter collection">
-        <input type="text" placeholder="Search Your Collection" id="filter collection" onChange={searchHandler} />
-      </label>
-      <label htmlFor="search_collection" />
-      <button id="search_collection" type="button" onClick={submitSearch}>
+      <label htmlFor="searchTitle" />
+      <button id="searchTitle" type="button" onClick={submitSearch}>
         <AiOutlineSearch />
       </button>
+
+      <label htmlFor="genre">
+        <select name="filters" id="genre" onChange={filterHandler}>
+          <option value="" disabled selected hidden>Choose a genre</option>
+          {genres.map((genre) => (
+            <option value={genre}>{genre}</option>
+          ))}
+        </select>
+      </label>
+      <label htmlFor="category">
+        <select name="filters" id="category" onChange={filterHandler}>
+          <option value="" disabled selected hidden>Choose a genre</option>
+          {categories.map(category => (
+            <option value={category}>{category}</option>
+          ))}
+        </select>
+      </label>
+      <label htmlFor="status">
+        <select name="filters" id="status" onChange={filterHandler}>
+          <option value="" disabled selected hidden>Choose a genre</option>
+          {statuses.map(status => (
+            <option value={status}>{status}</option>
+          ))}
+        </select>
+      </label>
+      <label htmlFor="platform">
+        <select name="filters" id="platform" onChange={filterHandler}>
+          <option value="" disabled selected hidden>Choose a genre</option>
+          {platforms.map(platform => (
+            <option value={platform}>{platform}</option>
+          ))}
+        </select>
+      </label>
 
     </div>
   );
