c90033ead791f5383d27c0bc6b3541b7253bcb51
Benjamin Thornton <benjamin.l.thornton@gmail.com>
2022-06-27 19:03:36 -0500

    Set up initial landing page with firebase authentication

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import UserMainContainer from '../containers/UserMainContainer.js';
+import LandingPageContainer from '../containers/LandingPageContainer';
+import UserMainContainer from '../containers/UserMainContainer';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserMainContainer />} />
+        <Route path="/" element={<LandingPageContainer />} />
+        <Route path="/UserMain" element={<UserMainContainer />} />
       </Routes>
     </Router>
   );
--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import {
+  query, collection, getDocs, where,
+} from 'firebase/firestore';
+import { auth, db, logout } from '../../firebase';
 import Login from './LogIn';
+import DemoSection from './DemoSection';
 
 function LandingPage() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
-
+  const [user, loading, error] = useAuthState(auth);
+  const [name, setName] = useState('');
+  const navigate = useNavigate();
+  const fetchUserName = async () => {
+    try {
+      const q = query(collection(db, 'users'), where('uid', '==', user?.uid));
+      const doc = await getDocs(q);
+      const data = doc.docs[0].data();
+      setName(data.name);
+    } catch (err) {
+      console.error(err);
+      alert('An error occured while fetching user data');
+    }
+  };
+  // useEffect(() => {
+  //   if (loading) return;
+  //   if (!user) return navigate('/');
+  //   fetchUserName();
+  // }, [user, loading]);
   return (
     <div>
-      <Login />
+      <div className="dashboard">
+        <div className="dashboard__container">
+          Logged in as
+          <div>{name}</div>
+          <div>{user?.email}</div>
+          <button type="button" className="dashboard__btn" onClick={logout}>
+            Logout
+          </button>
+        </div>
+        <DemoSection />
+        <Login />
+      </div>
     </div>
   );
 }
