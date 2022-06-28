cf251a92cd8c650486f3a11e1aeaf4fad946c351
kkranzfelder <kkranzfelder@gmail.com>
2022-06-28 19:46:26 -0400

    conflicts fixed for Brice pull request

--- client/src/components/UserMain/UserMain.jsx
+++ client/src/components/UserMain/UserMain.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-import MyCollection from './MyCollection/MyCollection';
-import ThreadsView from './Threads/ThreadsView';
-import Catalog from './PublicCatalog/Catalog.jsx';
-=======
 import CatalogContainer from '../../containers/CatalogContainer.js';
 import MyCollectionContainer from '../../containers/MyCollectionContainer.js';
->>>>>>> ae29445871c7e0c709788bf3750cd5bedb041e37
 
 function UserMain() {
   return (
     <div>
-<<<<<<< HEAD
-      <MyCollection />
-      <Catalog />
-      <ThreadsView />
-=======
       <MyCollectionContainer />
       <CatalogContainer />
->>>>>>> ae29445871c7e0c709788bf3750cd5bedb041e37
     </div>
   );
 }
