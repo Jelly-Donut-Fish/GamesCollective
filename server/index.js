8e2432e9d67e64c76b415db2285276f7d3c4ad45
Sonia Ann Friscia <soniaannfriscia@gmail.com>
2022-06-28 09:54:38 -0400

    get functioning server code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-const route = require('./routes');
+const router = require('./routes');
 const db = require('../database/index');
+require('dotenv').config();
 
 const app = express();
 
@@ -9,10 +10,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', '/client/dist')));
 app.use(express.json());
 
-app.use('*', route);
+app.use('/', router);
+
+db.poolConnect();
 
 app.listen(process.env.PORT, () => {
   console.log(`server listening on localhost:${process.env.PORT}`);
 });
-
-db.poolConnect();
--- server/routes.js
+++ server/routes.js
@@ -1,1 +1,80 @@
 const router = require('express').Router();
+// controller func imports
+const FUNC = (req, res) => {
+  console.log('put in a real function');
+  console.log(req.url, ' query params: ', req.query, 'params: ', req.params, 'body: ', req.body);
+  res.sendStatus(200);
+};
+
+// GAMES & COLLECTIONS
+router.get('/games', (req, res) => {
+  FUNC(req, res);
+});
+
+router.get('/games/:game_id', (req, res) => {
+  FUNC(req, res);
+});
+
+router.get('/games_users', (req, res) => {
+  FUNC(req, res);
+});
+
+router.post('/games_users', (req, res) => {
+  FUNC(req, res);
+});
+
+router.put('/games_users/status', (req, res) => {
+  FUNC(req, res);
+});
+
+router.put('/games_users/ratings', (req, res) => {
+  FUNC(req, res);
+});
+
+router.delete('/games_users', (req, res) => {
+  FUNC(req, res);
+});
+
+// COMMENTS
+router.post('/comments', (req, res) => {
+  FUNC(req, res);
+});
+
+router.put('/comments/:comment_id/report', (req, res) => {
+  FUNC(req, res);
+});
+
+// USERS
+router.post('/users', (req, res) => {
+  FUNC(req, res);
+});
+
+router.get('/users', (req, res) => {
+  FUNC(req, res);
+});
+
+router.put('/users', (req, res) => {
+  FUNC(req, res);
+});
+
+// CATEGORIES
+router.get('/categories', (req, res) => {
+  FUNC(req, res);
+});
+
+// GENRES
+router.get('/genres', (req, res) => {
+  FUNC(req, res);
+});
+
+// PLATFORMS
+router.get('/platforms', (req, res) => {
+  FUNC(req, res);
+});
+
+// LIBRARIES
+router.get('/libraries', (req, res) => {
+  FUNC(req, res);
+});
+
+module.exports = router;
