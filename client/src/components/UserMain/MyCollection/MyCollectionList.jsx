8adf6844b630cdc15fcb707e45e3112efc5a383f
Joy <parkerjn90@gmail.com>
2022-06-28 11:56:41 -0400

    write conditional rendering if passed props (TBD) are true

--- client/src/components/UserMain/MyCollection/MyCollectionList.jsx
+++ client/src/components/UserMain/MyCollection/MyCollectionList.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import MyCollectionTile from './MyCollectionTile';
 
-function MyCollectionList() {
+function MyCollectionList() { // import functions that take information about each game and push it up into state
+
   return (
     <div>
-      {/* [input].map(game => ( */}
-      <MyCollectionTile />
-      {/* )) */}
+      {[input].map(game => {
+        // call function here and pass in game.title, game.genre, game.category, game.status, game.platform
+        if (query !== undefined) {
+          var gameTitle = game.title.toLowerCase();
+          query = query.toLowerCase();
+          if (gameTitle.includes(query)) {
+            return  <MyCollectionTile />
+          }
+        }
+        if (genre !== undefined) {
+          if (game.genre === genre) {
+            return <MyCollectionTile />
+          }
+        }
+        if (category !== undefined) {
+          if (game.category === category) {
+            return <MyCollectionTile />
+          }
+        }
+        if (status !== undefined) {
+          if (game.status === status) {
+            return <MyCollectionTile />
+          }
+        }
+        if (platform !== undefined) {
+          if (game.platform === platform) {
+            return <MyCollectionTile />
+          }
+        }
+        if (query === undefined &&
+          genre === undefined &&
+          category === undefined &&
+          status === undefined) {
+            return <MyCollectionTile />
+          }
+})}
     </div>
   );
 }
