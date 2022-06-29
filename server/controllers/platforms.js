87545a0195234afb588cd950603794561a149d28
PO CHANG CHEN <kk741852963tw1610@gmail.com>
2022-06-30 07:17:05 +0800

    finish genre, category, platform model

--- server/controllers/platforms.js
+++ server/controllers/platforms.js
@@ -2,7 +2,7 @@ const platformModel = require('../../database/Models/platforms');
 const get = function (req, res) {
   platformModel.get()
     .then((data) => {
-      res.send(data);
+      res.send(data.rows[0]);
     })
     .catch((err) => {
       res.send(err.message);
