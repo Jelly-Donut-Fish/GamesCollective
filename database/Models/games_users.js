f3a7b09b0aa8b0c5c1447a16e3c77ba07718d9ba
Philip Koller <philipkoller@Philips-MacBook-Pro.local>
2022-06-29 15:34:31 -0500

    done with game_user model, get and post request to game_user work

--- database/Models/games_users.js
+++ database/Models/games_users.js
@@ -1,10 +1,11 @@
+/* eslint-disable camelcase */
 const { pool } = require('../index');
 
-const get = function (user_id) {
+const get = (user_id) => {
   const getUsersGames = `
   select((
     select json_build_object(
-      'id', $1,
+      'id', ${user_id},
       'steam_appid', g.steam_appid,
       'name', g.name,
       'developers', g.developers,
@@ -15,28 +16,39 @@ const get = function (user_id) {
       'website', g.website,
       'required_age', g.required_age
     )from games g where g.id = gu.game_id
-  ))from  game_user gu where gu.user_id = $1
+  ))from  game_user gu where gu.user_id = ${user_id}
 `;
-return pool.query(getUsersGames, user_id)
-}
+  // eslint-disable-next-line semi
+  return pool.query(getUsersGames)
+};
 
-const post = function (game_id, body) {
+const post = (user_id, body) => {
+  // eslint-disable-next-line no-param-reassign
+  body.status = body.status || 'want to play';
+  const {
+    game_id,
+    review,
+    rating,
+  } = body;
+
   const addGamesToCol = `
   insert into
   game_user (game_id, user_id, status, review, rating)
-  values($1, $2, $3, $4, $5)
+  values(${game_id}, ${user_id}, '${body.status}', '${review}', ${rating})
 `;
-}
+  // eslint-disable-next-line semi
+  return pool.query(addGamesToCol)
+};
 
-const putStatus = function (body) {
+const putStatus = (body) => {
   const changeStatus = `
   update game_user
   set status = $1
   where game_id = $2 and user_id = $3
 `;
-}
+};
 
-const putRatings = function (body) {
+const putRatings = (body) => {
   const editRatingReview = `
   update game_user
   set
@@ -44,18 +56,18 @@ const putRatings = function (body) {
     review = $2
   where game_id = $3 and user_id = $4
 `;
-}
+};
 
-const deleteGame = function (body) {
+const deleteGame = (body) => {
   const removeGameFromCol = `
   delete from game_user where user_id = $1 and game_id = $2
 `;
-}
+};
 
 module.exports = {
-  get: get,
-  post: post,
-  putStatus: putStatus,
-  putRatings: putRatings,
-  deleteGame: deleteGame
-}
+  get,
+  post,
+  putStatus,
+  putRatings,
+  deleteGame,
+};
