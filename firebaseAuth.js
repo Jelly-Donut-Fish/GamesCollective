72e8e167491dbb1fd8d04fb3efac82c4c3742af3
bricekoppin <bricekoppinesq@gmail.com>
2022-06-29 20:19:17 -0400

    Merge

---
+++ firebaseAuth.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCQsMZ7iOcG-zhOugv8y_PSlY4mqR_y2L0',
+  authDomain: 'games-collective-7a3bc.firebaseapp.com',
+  projectId: 'games-collective-7a3bc',
+  storageBucket: 'games-collective-7a3bc.appspot.com',
+  messagingSenderId: '873472952753',
+  appId: '1:873472952753:web:77fdf521bbf6c83b495590',
+  measurementId: 'G-3DCV4TKW4R',
+};
+
+module.exports = {
+  firebaseConfig,
+};
