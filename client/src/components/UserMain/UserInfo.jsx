146ed5973eb2b53fc7be628c3cf896ad5be70e27
kkranzfelder <kkranzfelder@gmail.com>
2022-07-01 10:35:04 -0400

    one last save before push.

--- client/src/components/UserMain/UserInfo.jsx
+++ client/src/components/UserMain/UserInfo.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 
 function UserInfo({ username, userphoto }) {
   return (
-<<<<<<< HEAD
-    <>
-      <img src={userphoto} alt={`${username}'s profile`} className="userProfilePic" />
-      <h3>{username}</h3>
-    </>
-=======
     <div className="userInfo">
       <div className="userImage">
         <img src={userphoto} alt={`${username}'s profile`} className="profilePic" />
@@ -16,7 +10,6 @@ function UserInfo({ username, userphoto }) {
         <h3 className="userName">{username}</h3>
       </div>
     </div>
->>>>>>> 8d397b36b52d1540ef155f6000abcf58c6a2989d
   );
 }
 
