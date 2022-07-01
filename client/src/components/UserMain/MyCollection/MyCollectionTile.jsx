94bc218c88d8930e4af071697272ae9d42951a09
Joy <parkerjn90@gmail.com>
2022-07-01 00:55:49 -0400

    styling for collection

--- client/src/components/UserMain/MyCollection/MyCollectionTile.jsx
+++ client/src/components/UserMain/MyCollection/MyCollectionTile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import moment from 'moment';
 import { MdComment } from 'react-icons/md';
 import { FaTrashAlt } from 'react-icons/fa';
@@ -15,7 +15,8 @@ function MyCollectionTile({
     if (confirm(`Are you sure you want to remove ${game.name} from your collection?`) === true) {
       removeFromCollection(index);
     }
-    axios.delete('/games_users', { data: { user_id: currentUser.site_id, game_id: game.id } })
+    console.log(currentUser.site_id, game.id)
+    axios.delete('/games_users', { params: { user_id: currentUser.site_id, game_id: game.id } })
       .then(() => {
         alert(`${game.name} was removed from your collection`);
       })
@@ -36,25 +37,33 @@ function MyCollectionTile({
   };
 
   return (
-    <div>
-      <div className="image_container">
+    <div className="collectionTile">
+      <div className="mainInfo">
         <img src={game.header_image} alt={`${game.name} thumbnail`} className="title_image" />
+        <h3 onClick={openGameView} className="mainTitle">{game.name}</h3><br></br>
+        <span className="mainDate">{moment(game.release_date).format("d MMM, YYYY")}</span><br></br>
+        <h4 className="mainDevelopers">{`Developer/Publisher: ${game.developers} ${game.publishers}`}</h4>
       </div>
-      <div className="info_container">
-        <h3 onClick={openGameView} className="title">{game.name}</h3>
-        <span className="date">{moment(game.release_date).format("d MMM, YYYY")}</span>
-        <h4>{`Developer/Publisher: ${game.developers} ${game.publishers}`}</h4><br></br>
-        <span>Platforms Available: </span>
-        {game.platforms.map((platform) => <span>{platform}</span>)}<br></br>
+      <div className="gameDetailsMini">
+        <span className="highlight">Platforms Available: </span>
+        {game.platforms.map((platform) => <span className="lowlight">{platform}</span>)}<br></br>
         <div className="features">
-          <span>Features:</span><br></br>
-          {game.genres.map((genre) => <p>{genre}</p>)}
-          {game.categories.map((category) => <p>{category}</p>)}
+          <span className="highlight">Features:</span><br></br>
+          {game.genres.map((genre) => <p className="lowlight">{genre}</p>)}
+          {game.categories.map((category) => <p className="lowlight">{category}</p>)}
         </div>
-        <p className="rating">{game.rating}</p>
-        <p className="status">{game.status}</p>
-        <span onClick={openThreadView} className="game_icon"><MdComment /></span>
-        <span onClick={removeGame} className="game_icon"><FaTrashAlt /></span>
+      </div>
+      <div className="moreInfo">
+        <div className="status">
+          <p className="status">{game.status}</p>
+        </div>
+        <div className="ratingMini">
+          <p className="rating">{game.rating}</p>
+        </div>
+        <div className="buttons">
+          <span onClick={openThreadView} className="game_icon"><MdComment /></span>
+          <span onClick={removeGame} className="game_icon"><FaTrashAlt /></span>
+        </div>
       </div>
     </div>
   );
