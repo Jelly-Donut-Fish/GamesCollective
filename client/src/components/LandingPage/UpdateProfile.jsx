defd2fc0b604c05df80c65c86eeeed931cfbebbf
Benjamin Thornton <benjamin.l.thornton@gmail.com>
2022-07-01 09:51:18 -0500

    Changed the nav bar

--- client/src/components/LandingPage/UpdateProfile.jsx
+++ client/src/components/LandingPage/UpdateProfile.jsx
@@ -32,7 +32,7 @@ function UpdatUserProfile({ currentUser, getUser }) {
   return (
     <div className="page">
       <nav className="nav-bar">
-        <h3 className="title">Games Collection</h3>
+        <h3 className="pageTitle">Games Collective</h3>
         <br />
         <Link className="link" to="/">Home</Link>
       </nav>
